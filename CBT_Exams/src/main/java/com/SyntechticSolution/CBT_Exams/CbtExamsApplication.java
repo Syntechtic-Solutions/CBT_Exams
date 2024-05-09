7d43d4adc428c5800a443a68699d9e1aea7c66a5
NnamdiThomas <osuohannamdithomas@gmail.com>
2024-05-09 02:12:24 +0100

    CBT Exam Code

---
+++ CBT_Exams/src/main/java/com/SyntechticSolution/CBT_Exams/CbtExamsApplication.java
@@ -0,0 +1,13 @@
+package com.SyntechticSolution.CBT_Exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbtExamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbtExamsApplication.class, args);
+	}
+
+}
---
+++ CBT_Exams/src/test/java/com/SyntechticSolution/CBT_Exams/CbtExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SyntechticSolution.CBT_Exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbtExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
